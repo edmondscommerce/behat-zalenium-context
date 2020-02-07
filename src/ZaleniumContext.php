326d8d1cfdd058d9eb1346e72e452e4b3b5252e1
Dan <Dan@EC>
2020-02-07 18:30:56 +0000

    Fixed cookie domain issue

--- src/ZaleniumContext.php
+++ src/ZaleniumContext.php
@@ -3,10 +3,7 @@
 namespace EdmondsCommerce\ZaleniumContext;
 
 use Behat\Behat\Hook\Scope\BeforeScenarioScope;
-use Behat\Mink\Driver\Selenium2Driver;
-use Behat\Mink\Mink;
 use Behat\MinkExtension\Context\RawMinkContext;
-use WebDriver\Session;
 
 class ZaleniumContext extends RawMinkContext
 {
@@ -25,14 +22,14 @@ class ZaleniumContext extends RawMinkContext
 
     /**
      * @param BeforeScenarioScope $scope
+     *
      * @BeforeScenario
      */
     public function setTestName(BeforeScenarioScope $scope)
     {
-        
+
         $driver = $this->getSession()->getDriver();
-        if ($driver instanceof ZaleniumDriver)
-        {
+        if ($driver instanceof ZaleniumDriver) {
             $driver->setName($scope->getScenario()->getTitle());
         }
     }
@@ -48,15 +45,14 @@ class ZaleniumContext extends RawMinkContext
 
     /**
      * @AfterScenario
+     *
      * @param \Behat\Behat\Hook\Scope\AfterScenarioScope $scope
      */
     public function reportTestStatus(\Behat\Behat\Hook\Scope\AfterScenarioScope $scope)
     {
-        if ($scope->getTestResult()->isPassed())
-        {
+        if ($scope->getTestResult()->isPassed()) {
             $this->reportSuccess();
-        } else
-        {
+        } else {
             $this->reportFailure();
         }
 
@@ -64,52 +60,8 @@ class ZaleniumContext extends RawMinkContext
     }
 
     /**
-     * Tries to set a cookie with out causing errors
-     * @param string $name
-     * @param string $value
-     * @throws \Exception
-     */
-    private function setCookie($name, $value)
-    {
-        try
-        {
-            // Don't continue if an alert box is open
-            if (!$this->isAlertOpen())
-            {
-                $this->getSession()->setCookie($name, $value);
-            }
-        } catch (\Exception $e)
-        {
-            //This only happens when we can't visit a page
-            if (strpos($e->getMessage(), 'unable to set cookie') === 0)
-            {
-                // Cookie exception, prevent it from stopping the test
-                echo $e->getMessage();
-                return;
-            }
-
-            throw $e;
-        }
-    }
-
-    private function isAlertOpen()
-    {
-        $driver = $this->getSession()->getDriver();
-        if ($driver instanceof \Behat\Mink\Driver\Selenium2Driver)
-        {
-            try
-            {
-                $driver->getWebDriverSession()->getAlert_text();
-            } catch (\WebDriver\Exception\NoAlertOpenError $e)
-            {
-                return false;
-            }
-            return true;
-        }
-    }
-
-    /**
      * @AfterStep
+     *
      * @param \Behat\Behat\Hook\Scope\AfterStepScope $scope
      */
     public function reportTestStep(\Behat\Behat\Hook\Scope\AfterStepScope $scope)
@@ -121,8 +73,66 @@ class ZaleniumContext extends RawMinkContext
     }
 
     /**
+     * Tries to set a cookie with out causing errors
+     *
+     * @param string $name
+     * @param string $value
+     *
+     * @throws \Exception
+     */
+    private function setCookie($name, $value)
+    {
+        try {
+            // Don't continue if an alert box is open
+            if (!$this->isAlertOpen()) {
+                $this->getSession()->setCookie($name, $value);
+            }
+        } catch (\Exception $e) {
+            if ($this->isSkippableException($e)) {
+                return;
+            }
+
+            throw $e;
+        }
+    }
+
+    private function isSkippableException(\Exception $exception): bool
+    {
+        // Cookie errors only happen when we can't visit a page or we are not yet on the desired site in a step
+        $skipErrors = [
+            'unable to set cookie',
+            'invalid cookie domain' // You can't set a cookie when you are not on a site
+        ];
+
+        foreach ($skipErrors as $skipError) {
+            if (strpos($exception->getMessage(), $skipError) === 0) {
+                echo $exception->getMessage();
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function isAlertOpen()
+    {
+        $driver = $this->getSession()->getDriver();
+        if ($driver instanceof \Behat\Mink\Driver\Selenium2Driver) {
+            try {
+                $driver->getWebDriverSession()->getAlert_text();
+            } catch (\WebDriver\Exception\NoAlertOpenError $e) {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    /**
      * @param string $testName
      * @param string $content
+     *
      * @throws \Exception#
      */
     private function reportStep($testName, $content)
